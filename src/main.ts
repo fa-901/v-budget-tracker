a33413051af43fc1dcc9b7096c93591527018379
Farhan Mohd. Fokrul Alam <farhan.mohd@brainstation-23.com>
2023-01-24 11:12:04 +0600

    add bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 
-import "./assets/main.css";
+import "./assets/main.scss";
 
 const app = createApp(App);
 
